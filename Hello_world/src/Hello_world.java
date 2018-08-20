9b3db7ba05287ac8ed905ea8dd15de06622bdf91
Aidan Lee <meow123al@gmail.com>
2018-08-20 09:42:22 -0700

    -v1.0(First commit)
    Added Hello_world.java that prints "Hello World! From Aidan Lee" to the
    console.

---
+++ Hello_world/src/Hello_world.java
@@ -0,0 +1,15 @@
+/**
+ * This program prints to the console "Hello World! From Aidan Lee
+ * 
+ * @aidanlee(your name) 
+ * @1.0(a version number or a date)
+ */
+public class Hello_world
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Aidan Lee"); //Display the string
+    }
+}
+
+
