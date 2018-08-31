d2c08bf095d23e0ff5c021d00f526f7e4e37d14b
Aidan Lee <Aidan Lee@DESKTOP-AS73H75>
2018-08-31 09:50:31 -0700

    v1.0 Added Pikachu Code. Not complete

---
+++ PikachuCode/src/PikachuRunner.java
@@ -0,0 +1,20 @@
+
+/*Prints this:
+ *  Pikachu welcomes you to the world of Pokemon!
+                (\__/)
+                (o^.^)
+              z(_(")(")
+ * @author Aidan Lee
+ * @1.0 (8/31/2018)
+ */
+
+
+public class PikachuRunner {
+	public static void main(String [] args) {
+		System.out.println("Pikachu welcomes you to the world of Pokemon!\r\n" + "");
+		System.out.println("\t\t  (\\__/)\r\n" + "");
+		System.out.println("\t\t  (o^.^)\r\n" + "");
+		System.out.println("\t\t  tz(_(\")(\")");
+	}
+
+}
