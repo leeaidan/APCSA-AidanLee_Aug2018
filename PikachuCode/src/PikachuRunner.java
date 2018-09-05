029be2bb00421440c68371b512a079a5e034eafb
Aidan Lee <Aidan Lee@DESKTOP-AS73H75>
2018-09-05 09:35:40 -0700

    v1.1 Fixed incorrect position of pikachu tail

--- PikachuCode/src/PikachuRunner.java
+++ PikachuCode/src/PikachuRunner.java
@@ -14,7 +14,7 @@ public class PikachuRunner {
 		System.out.println("Pikachu welcomes you to the world of Pokemon!\r\n" + "");
 		System.out.println("\t\t  (\\__/)\r\n" + "");
 		System.out.println("\t\t  (o^.^)\r\n" + "");
-		System.out.println("\t\t  tz(_(\")(\")");
+		System.out.println("\t\tz(_(\")(\")");
 	}
 
 }
