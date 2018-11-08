3d3aa0079e4d6e1e4b43e4f55385b612eaa1d4d6
BobaTracker <bobatrackerwebsite@gmail.com>
2018-11-08 08:41:58 -0800

    Added ArraysMiniLab

---
+++ ArrayMiniLab/src/LotsOfCopies.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class LotsOfCopies {
+	public static void main(String []args) {
+		int num =7;
+		String strMain = "APCS";
+		int[] arrMain = {1,2,3,4,5};
+		changeMe(num, strMain, arrMain);
+		System.out.println("num: "+ num);
+		System.out.println("strMain: "+ strMain);
+		System.out.println("arrMain: "+ Arrays.toString(arrMain));
+		
+		int a=5;
+		int b=a;
+		a=7;
+		System.out.println(b);
+				
+		
+		
+	}
+	
+	public static void changeMe(int x, String str, int[] arr) {
+		x++;
+		str +=str;
+		arr[2]=42;
+		
+	}
+
+}
